f08dbc1357df1683067cf782c6443d776b57b7ad
MrSullivan <>
2021-05-16 21:10:48 +0000

    lots

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #write your code below this line
+    print_until_number(5)
+
+def print_until_number(number):
+  i = 1
+  while (i <= number):
+      print(i)
+      i += 1
+
+   
 
 if __name__ == '__main__':
     main()
